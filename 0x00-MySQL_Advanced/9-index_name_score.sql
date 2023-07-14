3390c04ea6c3444790d6b60a7117ee2f7925bb67
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-07-14 23:18:30 +0000

    Update 9-index_name_score to use LEFT to return the first letter of name and score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,2 @@
 -- Creates an index indx_name_first_score on table names and the first letter of name and score
-CREATE INDEX idx_name_first_score ON names (name(1), score(1));
+CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), LEFT(score, 1));
