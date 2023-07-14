dba6f548a3aa99bc951c254ba5a8f23391971859
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-07-14 23:02:30 +0000

    Upload 7-average_score.sql

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,12 @@
+-- Creates a stored procedure ComputeAverageScoreForUse
+
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+	DECLARE average_score FLOAT;
+	SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
+	UPDATE users
+	SET average_score = average_score WHERE id = user_id;
+END //
+
+DELIMITER ;
